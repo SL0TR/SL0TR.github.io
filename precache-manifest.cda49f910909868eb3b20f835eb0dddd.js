39fb91529f0f66b3072766e1b111eb4fd11ff3c1
SL0TR <mohaiminislam94@gmail.com>
2019-02-09 01:19:44 +0600

    Updates

---
+++ precache-manifest.cda49f910909868eb3b20f835eb0dddd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c7b5ff158b0af23db2b",
+    "url": "/static/js/main.8c7b5ff1.chunk.js"
+  },
+  {
+    "revision": "787274b0fc90d4d765cc",
+    "url": "/static/js/1.787274b0.chunk.js"
+  },
+  {
+    "revision": "13a7010bf817f1f419aead0b9c3d670b",
+    "url": "/index.html"
+  }
+];
